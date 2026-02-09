abc9661c3cb3cf0e6b163ff0b075369fbb84f876
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-02-09 15:06:33 -0600

    added Constraint library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    //ConstraintLayout
+    implementation(libs.constraint.layout)
+
+
 }
