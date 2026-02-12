ec92c7ae6e52c42c7af38cc9119ce785e36211a5
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-02-11 20:51:10 -0600

    drawer item with properties

---
+++ app/src/main/java/com/gilorroristore/jetpackcompose2026/model/DrawerItem.kt
@@ -0,0 +1,9 @@
+package com.gilorroristore.jetpackcompose2026.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class DrawerItem(
+    val title: String,
+    val icon: ImageVector,
+    val notification: Int
+)
