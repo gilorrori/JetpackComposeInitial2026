17ff0a8deb19934da4eb3f79fcdb223a3b723a03
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-02-17 17:11:53 -0600

    Navigation with extras

--- app/src/main/java/com/gilorroristore/jetpackcompose2026/components/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/gilorroristore/jetpackcompose2026/components/navigation/NavigationWrapper.kt
@@ -1,10 +1,11 @@
 package com.gilorroristore.jetpackcompose2026.components.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.gilorroristore.jetpackcompose2026.components.navigation.screensexamples.DetailScreen
 import com.gilorroristore.jetpackcompose2026.components.navigation.screensexamples.HomeScreen
 import com.gilorroristore.jetpackcompose2026.components.navigation.screensexamples.LoginScreen
 
@@ -17,7 +18,19 @@ fun NavigationWrapper() {
         }
 
         composable<Home> {
-            HomeScreen() { navController.popBackStack() }
+            HomeScreen(
+                navBack = { navController.popBackStack() },
+                navToDetail = { id, showID ->
+                    navController.navigate(Detail(id = id, showID = showID))
+                })
+        }
+
+        composable<Detail> {navBackStackEntry ->
+            val detail = navBackStackEntry.toRoute<Detail>()
+
+            DetailScreen(detail.id, detail.showID) {
+                navController.navigate(Login)
+            }
         }
     }
 }
--- app/src/main/java/com/gilorroristore/jetpackcompose2026/components/navigation/screensexamples/HomeScreen.kt
+++ app/src/main/java/com/gilorroristore/jetpackcompose2026/components/navigation/screensexamples/HomeScreen.kt
@@ -1,20 +1,33 @@
 package com.gilorroristore.jetpackcompose2026.components.navigation.screensexamples
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun HomeScreen(navBack: () -> Unit) {
+fun HomeScreen(navBack: () -> Unit, navToDetail: (String, Boolean) -> Unit) {
+    var text by remember { mutableStateOf("") }
+
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Spacer(modifier = Modifier.weight(1f))
         Text(text = "Home", fontSize = 30.sp)
+        Spacer(modifier = Modifier.weight(1f))
+        Row() {
+            TextField(value = text, onValueChange = { text = it }, modifier = Modifier.weight(1f))
+            Button(onClick = { navToDetail(text, true) }) { Text(text = "Detail") }
+        }
         Spacer(modifier = Modifier.weight(1f))
         Button(
             onClick = {
