ffe8fcacc45c586a3a0e25af018f1128bec66fed
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-02-17 13:49:09 -0600

    Navigation with compose

---
+++ app/src/main/java/com/gilorroristore/jetpackcompose2026/components/navigation/NavigationWrapper.kt
@@ -0,0 +1,23 @@
+package com.gilorroristore.jetpackcompose2026.components.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.gilorroristore.jetpackcompose2026.components.navigation.screensexamples.HomeScreen
+import com.gilorroristore.jetpackcompose2026.components.navigation.screensexamples.LoginScreen
+
+@Composable
+fun NavigationWrapper() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Login) {
+        composable<Login> {
+            LoginScreen() { navController.navigate(Home) }
+        }
+
+        composable<Home> {
+            HomeScreen() { navController.popBackStack() }
+        }
+    }
+}
---
+++ app/src/main/java/com/gilorroristore/jetpackcompose2026/components/navigation/screensexamples/LoginScreen.kt
@@ -0,0 +1,33 @@
+package com.gilorroristore.jetpackcompose2026.components.navigation.screensexamples
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreen(navigate: () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize().background(Color.Red.copy(alpha = 0.2f)), horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = "L O G I N", fontSize = 30.sp)
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            onClick = {
+                navigate()
+            }
+        ) {
+            Text(
+                text = "Navegar"
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+
+    }
+}
