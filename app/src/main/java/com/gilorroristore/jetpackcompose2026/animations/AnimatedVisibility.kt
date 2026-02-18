6b5ae6a6be06f401942dd7d6b19bab4e77bca940
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-02-18 14:13:07 -0600

    Animations for composable, only changing a "IF" by "AnimatedVisibility"

---
+++ app/src/main/java/com/gilorroristore/jetpackcompose2026/animations/AnimatedVisibility.kt
@@ -0,0 +1,59 @@
+package com.gilorroristore.jetpackcompose2026.animations
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MyAnimatedVisibility(modifier: Modifier = Modifier) {
+    var showView by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Button(
+            onClick = { showView = !showView }
+        ) {
+            Text(text = "Presioname")
+        }
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        AnimatedVisibility(
+            visible = showView,
+            enter = scaleIn(),
+            exit = scaleOut()
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .background(Color.Red)
+            )
+        }
+    }
+}
