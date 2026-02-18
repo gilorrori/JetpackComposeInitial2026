2609b8a87b150e0b08d30bf1a5484182b2f09dad
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-02-17 18:01:47 -0600

    Settings model and parcelable fixed and generic

---
+++ app/src/main/java/com/gilorroristore/jetpackcompose2026/components/navigation/types/SettingsModelType.kt
@@ -0,0 +1,31 @@
+package com.gilorroristore.jetpackcompose2026.components.navigation.types
+
+import android.net.Uri
+import android.os.Build
+import androidx.navigation.NavType
+import androidx.savedstate.SavedState
+import com.gilorroristore.jetpackcompose2026.components.navigation.screensexamples.model.SettingModel
+import kotlinx.serialization.json.Json
+
+val settingsModelType = object : NavType<SettingModel>(isNullableAllowed = true){
+    override fun put(bundle: SavedState, key: String, value: SettingModel) {
+        bundle.putParcelable(key, value)
+    }
+
+    override fun get(bundle: SavedState, key: String): SettingModel? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            bundle.getParcelable(key, SettingModel::class.java)
+        } else {
+            bundle.getParcelable(key)
+        }
+    }
+
+    override fun parseValue(value: String): SettingModel {
+        return Json.decodeFromString<SettingModel>(value)
+    }
+
+    override fun serializeAsValue(value: SettingModel): String {
+        return Uri.encode(Json.encodeToString(value))
+    }
+
+}
